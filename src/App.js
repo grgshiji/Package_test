6ef85c8d92581b42e59ab44567a115ff458d5f4a
grgshiji <shiji@neoito.com>
2023-09-05 15:38:53 +0530

    [commitHash:6337040]

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Ui_element_block from './remote/UI_Element_Block'
+
+export default function App() {
+  return (
+    <div className="App" data-testid="app">
+    <Ui_element_block/>
+    </div>
+  )
+}
